c9903b851bf8c86bfb9686e31440d5feb55fc291
mikachi20 <mKs1m400H@gmail.com>
2022-08-27 19:32:06 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// 必要な関数を import
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxlW4Imesx3G1tPQthyiG3eRvMYOsNiQg",
+  authDomain: "team3-originalapp.firebaseapp.com",
+  projectId: "team3-originalapp",
+  storageBucket: "team3-originalapp.appspot.com",
+  messagingSenderId: "106853649958",
+  appId: "1:106853649958:web:b3856c8056f265f21160e7",
+  measurementId: "G-9JKED523FG",
+}
+
+// Firebaseアプリオブジェクトを初期化
+const app = initializeApp(firebaseConfig)
+// Firestoreを読み込み、db(databaseの略)として export
+export const db = getFirestore(app)

cdef46b2231d5553156ca612f558fd83b7a8e4a4
bear000660 <mnstapazuru661@gmail.com>
2022-08-27 21:23:52 +0900

    文末セミコロンを省略する設定にしました。

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,11 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: ["error", "never", { beforeStatementContinuationChars: "never" }],
+    "semi-spacing": ["error", { after: true, before: false }],
+    "semi-style": ["error", "first"],
+    "no-extra-semi": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error",
   },
 }

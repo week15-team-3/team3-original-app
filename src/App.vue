1f0c35f11e5d26e7027c2319efaec1bea113f3b8
Futa Ando <futa990628@gmail.com>
2022-08-30 19:20:37 +0900

    router修正

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/maincalendar">Calendar</router-link> |
+    <router-link to="/salary">Salary</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
+// import { shallowReadonly } from "vue"
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import SalaryView from "../views/SalaryView.vue"
+import MainCalendar from "../views/MainCalendar.vue"
 
 const routes = [
   {
@@ -8,11 +11,18 @@ const routes = [
     component: HomeView,
   },
   {
+    path: "/maincalendar",
+    name: "calendar",
+    component: MainCalendar,
+  },
+  {
+    path: "/salary",
+    name: "salary",
+    component: SalaryView,
+  },
+  {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },

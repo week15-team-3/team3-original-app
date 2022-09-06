fbce64d91533373e270c5da258b10d6f8fff2bf0
bear000660 <mnstapazuru661@gmail.com>
2022-09-06 11:42:51 +0900

    バイト先登録ページの基本を完成させました。

--- src/views/RegisterWorkView.vue
+++ src/views/RegisterWorkView.vue
@@ -1,7 +1,42 @@
 <template>
   <h1>バイト先の登録</h1>
+  <div class="job-input-area">
+    <div id="job_name-input-area">
+      名前：
+      <input class="job-input-field" v-model="inputJobName" />
+    </div>
+    <div id="job_wage-input-area">
+      時給:
+      <input class="job-input-field" v-model="inputJobWage" />円
+    </div>
+  </div>
+  <button class="register-button" @click="registerWork">バイト先を登録</button>
+  <h3>バイト先一覧</h3>
+  <div class="job-container" v-for="(Job, index) in Jobs" :key="index">
+    <div class="job-display-name">{{ Job.name }}</div>
+    <div class="job-display-wage">{{ Job.wage }}円</div>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      inputJobName: "",
+      inputJobWage: "",
+      Jobs: [],
+    }
+  },
+  methods: {
+    registerWork() {
+      this.Jobs.push({ name: this.inputJobName, wage: this.inputJobWage })
+    },
+  },
+}
+</script>
 
-<style></style>
+<style>
+.job-container {
+  border: 3px solid;
+}
+</style>

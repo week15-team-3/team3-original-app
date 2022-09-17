9e508ac365d27cb157081a41481b28d9378e93e5
KotaOmatsu <okota1218@icloud.com>
2022-09-17 17:13:36 +0900

    コンフリクト解消

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="wrapper">
+  <div class="wrapper">
     <div v-if="logInOut === false">
       <div class="container">
         <h2>Welcome</h2>
@@ -19,10 +19,7 @@
           <button @click="SignOut" class="loginbutton">ログアウト</button>
         </div>
         <nav>
-          <router-link to="/">Home</router-link>
           <router-link to="/maincalendar">calendar</router-link>
-          <!-- <router-link to="/salary">salary</router-link> -->
-          <!-- <router-link to="/about">About</router-link> -->
         </nav>
         <router-view />
       </div>
@@ -43,8 +40,6 @@
   </div>
 </template>
 
-
-<style scoped>
 <script>
 import {
   getAuth,
@@ -138,7 +133,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 @import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300);
 
 * {
